87e55ee127a8d4ef6f71dfd84ec747de3e3bdcd3
Rossi <rossi.a.rafeh@gmail.com>
2016-11-15 00:47:13 +0100

    update setup, clearer

--- ps4prep-st.R
+++ ps4prep-st.R
@@ -9,18 +9,18 @@
 # 6. Directory name : Econometrics4Law16
 # 7. Create as a sub-directory of : Browse -> to YOURDRIVELOCATION
 # 7bis. Create Project
-# 8. Open a new .R file, and type the command (don't copy), keep the cursor on the same line and click on your keyboard CTRL + ENTER (or CMD + ENTER on mac): 
+# 8. Open a new .R file, save as ps4ex1.R  ; In it, type the following command : 
 getwd()
+# 8bis. Did you copy the command? I told you to type. Erase and type. Save by clicking CTRL + S (or CMD + S on mac), keep the cursor on the same command line and click on your keyboard CTRL + ENTER (or CMD + ENTER on mac).
 # 9. It should give you a character string output that ends with "Econometrics4Law16"
 # Example output of 9 on my mac : "/Users/rossi/Dropbox/Econometrics4Law16"
-# 9bis. Google if you do not know what's "character string", or "output"
+# CTRL + ENTER runs the line the cursor is in; it's an RStudio trick, not an R trick
+# 9bis. Google if you do not know what's "character string", or "output". or the difference between Rstudio and R
 # 10. In Rstudio, click on Tools > Global Options > Code : check the case "Soft-wrap R source files"
 # 11. In Rstudio, click on Tools > Global Options > Pane Layout : click on the top left pane, change to source; click on top right pane, and change to console
 # 12. Google if you do not know what is source, what is console
-
 # 13. If you're using your own computers run the commands :
 install.packages("AER")
 install.packages("foreign")
-
-# 14. Create a new .R file, and call it ps4ex1.R
+# 14. Google what's an R package, and what do AER and foreign packages contain
 # 15. On github, locate ps4ex1-st.R : you'll take the commands from there, but you'll type them and you will not copy them.
